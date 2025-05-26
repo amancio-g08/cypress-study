28d0e8d2aee98c86935eab48ca20b2ef69855eb5
Gabriel Amâncio <gabriel_amancio@hotmail.com>
2025-05-25 22:50:34 -0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
 
-    baseUrl: 'https://pdvtechat-orangehrm.xn7egy.easypanel.host/web/index.php'
+    baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php'
   },
 });
